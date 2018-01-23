a3fc58db2b7a3e0a9dcb1645ea3cb4ff2aad297e
Nicolas <nicolas@mgig.fr>
2018-01-23 06:43:11 +0100

    Première version
    Script de maintenance et de sauvegarde de la base de données en local. À lancer tous les jours avec un cron.

---
+++ tools/db.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+DATE=`date +%Y-%m-%d`
+
+# Script d'optimisation et de sauvegarde de la base de données. Repose sur WP-CLI. 
+
+# Optimisations (basé sur https://gist.github.com/lukecav/66f1039edcd2827fd1bde82dce86a2be)
+wp transient delete --expired 
+wp cache flush
+wp post delete $(wp post list --post_type='revision' --format=ids)
+wp db optimize
+
+# Sauvegarde
+wp db export /home/backup/$DATE.sql
+
+# Nettoyage des anciennes sauvegardes (5 derniers jours)
+find /home/backup/ -maxdepth 1 -type f -mmin +7200 | xargs rm -rf
