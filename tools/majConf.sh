b7c5004c7b3e86ef7f0168f573a4abf06be6354b
Nicolas <nicolas@mgig.fr>
2018-03-18 22:05:24 +0100

    Fix

--- tools/majConf.sh
+++ tools/majConf.sh
@@ -2,7 +2,7 @@
 
 # Script qui permet de mettre à jour la configuration depuis le dépôt et d'installer les fichiers au bon endroit.
 
-cd /root/config/
+cd ~/config/
 git pull origin master
 
 cp -rf etc/caddy/Caddyfile /etc/caddy/
