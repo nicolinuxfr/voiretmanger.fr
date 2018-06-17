b434ee8d0d420657d487c8adb4ecef181fe66bf0
Nicolas <nicolas@mgig.fr>
2018-06-17 11:44:57 +0200

    Correction chemin wp

--- tools/db.sh
+++ tools/db.sh
@@ -6,13 +6,13 @@ DATE=`date +%Y-%m-%d`
 echo "Optimisation et sauvegarde de la base de données : $DATE"
 
 # Optimisations (basé sur https://gist.github.com/lukecav/66f1039edcd2827fd1bde82dce86a2be)
-wp transient delete --expired 
-wp cache flush
-wp post delete $(wp post list --post_type='revision' --format=ids)
-wp db optimize
+/usr/local/bin/wp transient delete --expired 
+/usr/local/bin/wp cache flush
+/usr/local/bin/wp post delete $(/usr/local/bin/wp post list --post_type='revision' --format=ids)
+/usr/local/bin/wp db optimize
 
 # Sauvegarde
-wp db export ~/backup/$DATE.sql
+/usr/local/bin/wp db export ~/backup/$DATE.sql
 
 # Nettoyage des anciennes sauvegardes (5 derniers jours)
 find ~/backup/ -maxdepth 1 -type f -mmin +7200 | xargs rm -rf
