629d5fbe5f26fbc13aa47640d32b7f7e876534b1
Nicolas F <nicolinux@gmail.com>
2019-06-10 11:43:52 +0200

    WP en root
    C'est pas une bonne idée en théorie, mais pour mes besoins, ça va bien…

--- tools/db.sh
+++ tools/db.sh
@@ -6,13 +6,13 @@ DATE=`date +%Y-%m-%d`
 echo "Optimisation et sauvegarde de la base de données : $DATE"
 
 # Optimisations (basé sur https://gist.github.com/lukecav/66f1039edcd2827fd1bde82dce86a2be)
-/usr/local/bin/wp transient delete --expired 
-/usr/local/bin/wp cache flush
-/usr/local/bin/wp post delete $(/usr/local/bin/wp post list --post_type='revision' --format=ids)
-/usr/local/bin/wp db optimize
+/usr/local/bin/wp --allow-root transient delete --expired 
+/usr/local/bin/wp --allow-root cache flush
+/usr/local/bin/wp --allow-root post delete $(/usr/local/bin/wp post list --post_type='revision' --format=ids)
+/usr/local/bin/wp --allow-root db optimize
 
 # Sauvegarde
-/usr/local/bin/wp db export ~/backup/$DATE.sql
+/usr/local/bin/wp --allow-root db export - | gzip ~/backup/$DATE.sql.gz
 
 # Nettoyage des anciennes sauvegardes (5 derniers jours)
 find ~/backup/ -maxdepth 1 -type f -mmin +7200 | xargs rm -rf
