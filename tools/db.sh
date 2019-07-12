2899da4200add359eb40bac51c00d96645c0aa0b
Nicolas <nicolas@mgig.fr>
2019-07-12 10:09:19 +0200

    DB : nettoyage révisions
    Le paramètre --force est nécessaire désrmais

--- tools/db.sh
+++ tools/db.sh
@@ -8,7 +8,7 @@ echo "Optimisation et sauvegarde de la base de données : $DATE"
 # Optimisations (basé sur https://gist.github.com/lukecav/66f1039edcd2827fd1bde82dce86a2be)
 /usr/local/bin/wp --allow-root transient delete --expired 
 /usr/local/bin/wp --allow-root cache flush
-/usr/local/bin/wp --allow-root post delete $(/usr/local/bin/wp --allow-root post list --post_type='revision' --format=ids)
+/usr/local/bin/wp --allow-root post delete --force $(/usr/local/bin/wp --allow-root post list --post_type='revision' --format=ids)
 /usr/local/bin/wp --allow-root db optimize
 
 # Sauvegarde
