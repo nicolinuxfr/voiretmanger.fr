3dec31a6fc0eac1f3f51c527040b6981d51499f3
Nicolas <nicolas@mgig.fr>
2020-02-14 21:39:37 +0100

    BRanche pas master

--- tools/majConf.sh
+++ tools/majConf.sh
@@ -3,7 +3,7 @@
 # Script qui permet de mettre à jour la configuration depuis le dépôt et d'installer les fichiers au bon endroit.
 
 cd ~/config/
-git pull origin master
+git pull origin
 
 cp -rf etc/caddy/Caddyfile /etc/caddy/
 chown caddy:caddy /etc/caddy/Caddyfile
