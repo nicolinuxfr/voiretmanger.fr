29d957011e23afd44be0e5ec69c9f05ff3844a2e
Nicolas <nicolas@mgig.fr>
2020-02-15 11:14:25 +0100

    Nettoyage

--- tools/update.sh
+++ tools/update.sh
@@ -7,8 +7,5 @@ curl https://getcaddy.com | bash -s personal
 setcap cap_net_bind_service=+ep $(which caddy)
 service caddy restart
 
-# Micro
-cd /usr/local/bin; curl https://getmic.ro | bash 
-
 # WP-CLI
 wp cli update
