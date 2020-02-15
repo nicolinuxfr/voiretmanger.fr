e32709e4ceb092662d01a7d704990dafd4761120
Nicolas <nicolas@mgig.fr>
2020-02-15 11:02:56 +0100

    Correction

--- tools/post-install.sh
+++ tools/post-install.sh
@@ -23,7 +23,7 @@ read passwddb
 
 mysql -u root -p$passwdroot <<EOF
 CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-GRANT ALL ON wordpress.* TO 'voiretmanger'@'localhost' IDENTIFIED BY 'c60fc45b350776cb';
+GRANT ALL ON wordpress.* TO 'voiretmanger'@'localhost' IDENTIFIED BY '$passwddb';
 USE wordpress;
 SOURCE /root/dump.sql;
 FLUSH PRIVILEGES;
