35274441d6afedd93cb2c071b4db84ae77b79131
Nicolas <nicolas@mgig.fr>
2020-05-05 07:59:44 +0200

    Cady 2.0.0

--- tools/update-caddy.sh
+++ tools/update-caddy.sh
@@ -4,7 +4,7 @@
 
 # Caddy
 ## Vérifier version ici : https://github.com/caddyserver/caddy/releases
-VERSION="2.0.0-rc.3"
+VERSION="2.0.0"
 cd /tmp/
 curl --retry 5 -LO https://github.com/caddyserver/caddy/releases/download/v$VERSION/caddy_${VERSION}_linux_amd64.tar.gz
 tar -xzf caddy_*
